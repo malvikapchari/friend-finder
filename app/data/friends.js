2fe11c6518376cdd194e37b8071f177017747ee4
Malvika Patrachari <malvikapatrachari@Malvikas-MBP-2.home>
2018-08-09 20:05:38 -0400

    FInished ish

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+
+var friends = [
+	{
+		name: 'Krithika Patel',
+		photo: 'https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi6o-fTleHcAhWIneAKHb0NAnsQjRx6BAgBEAU&url=https%3A%2F%2Fclassroomclipart.com%2Fclipart%2FClipart%2FCartoons.htm&psig=AOvVaw0fNUef7fpu7idohDfsLav9&ust=1533945290696416',
+		scores: [3, 5, 4, 4, 3, 5, 5, 1, 3, 5]
+	},{
+		name: 'Sanika Boa',
+		photo: 'http://getdrawings.com/cliparts/cartoon-characters-clipart-1.jpg',
+		scores: [2, 3, 3, 5, 1, 1, 2, 5, 1, 1]
+    },{
+		name: 'Katie Smith',
+		photo: 'https://i.pinimg.com/originals/1a/48/44/1a4844c77636604e55cb91cebc64563d.jpg',
+		scores: [4, 3, 4, 2, 2, 2, 3, 5, 4, 3]
+	},
+    {
+		name: 'Zara S',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeuIPeys0ld32ozii7fIZvZiFKslLnbuHXGm_klwEcQAP-Y33Jgw',
+		scores: [4, 3, 4, 2, 2, 2, 3, 5, 4, 3]
+	},{
+		name: 'Matt Davis',
+		photo: 'http://typegoodies.me/wp-content/uploads/6007477-cartoon-characters-clipart-images-of-691x1024.png',
+		scores: [2, 1, 5, 2, 4, 1, 1, 4, 2, 1]
+	}];
+
+module.exports = friends;
